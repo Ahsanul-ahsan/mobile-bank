30bb127b4a24c1397729839487782965c77a652b
Ahsanul-ahsan <ahsanul714@gmail.com>
2026-02-18 17:08:29 +0600

    home page setup

---
+++ Java script/java-script.js
@@ -0,0 +1,16 @@
+   
+//    for login file 
+   document.getElementById('input-btn').addEventListener('click', function(){
+        const inputNumber = document.getElementById('input-number');
+        const contactNumber = inputNumber.value; 
+        const inputpin = document.getElementById('input-pin');
+        const pin = inputpin.value;
+        if(contactNumber == "01705717651" && pin == "1234"){
+            alert('login sucess');
+            window.location.assign("/home.html")
+        }
+        else{
+            alert('your password worng');
+            return;
+        }
+    })
